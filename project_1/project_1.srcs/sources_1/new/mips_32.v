4ae9e12ec1ccf666a7f32b07f7c9d8027a7e35c4
Shau <38shaurya@gmail.com>
2025-03-21 16:33:50 +0530

    jump

--- project_1/project_1.srcs/sources_1/new/mips_32.v
+++ project_1/project_1.srcs/sources_1/new/mips_32.v
@@ -1,16 +1,18 @@
 `timescale 1ns / 1ps
-module mips_32(clk,we3,we,op,sel);
-input clk,we3,we,sel;
+module mips_32(clk,we3,we,op,sel,j);
+input clk,we3,we,sel,j;
 input [2:0]op;
-wire [31:0]pc,instr,rdreg1,rdreg2,sim,alres1,alres2,alres3,d_out,m1r,m2r,m3r,m4r,m5r,pro;
+wire [31:0]pc,instr,rdreg1,rdreg2,sim,alres1,alres2,alres3,d_out,m1r,m2r,m3r,m4r,m5r,pro1,pro2;
 wire [4:0]mr;
 wire zf,selm;
 
-prog_counter pc1 (.pco(pc),.clk(clk),.pci(m3r));
+assign pro2 = instr[25:0] << 2;
+prog_counter pc1 (.pco(pc),.clk(clk),.pci(m4r));
 alu_behaviour a2 (.a(pc),.b(8'h00000004),.op(3'b000),.o(alres2));
-l_shift l1 (.in(sim),.out(pro));
-alu_behaviour a3 (.a(pro),.b(alres2),.op(3'b000),.o(alres3));
+l_shift l1 (.in(sim),.out(pro1));
+alu_behaviour a3 (.a(pro1),.b(alres2),.op(3'b000),.o(alres3));
 mux_32 m3 (.a(alres3),.b(alres2),.sel(selm),.o(m3r));
+mux_32 m4 (.a(m3r),.b({alres2[31:28],pro2}),.sel(j),.o(m4r));
 ins_mem im1 (.a(pc),.rd(instr));
 p_mux p1 (.a(instr[20:16]),.b(instr[15:11]),.sel(sel),.o(mr));
 reg_file rf1 (.a1(instr[25:21]),.a2(instr[20:16]),.a3(mr),.rd1(rdreg1),.rd2(rdreg2),.wd3(m2r),.clk(clk),.we3(we3));
